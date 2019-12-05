886eba0f165cf3aeb342c85b8691e4ca3b8eef90
MilanFox <milan.fox@linuxmail.org>
2019-12-05 18:00:00 +0100

    19/05 - Sunny with a Chance of Asteroids

---
+++ src/2019/day_05.mjs
@@ -0,0 +1,9 @@
+import * as fs from 'node:fs';
+import { IntCodeComputer } from './utils/useIntCodeInterpreter.mjs';
+
+const memory = fs.readFileSync('input.txt', 'utf-8').trim().split(',').map(n => parseInt(n));
+
+const computer = new IntCodeComputer(memory);
+await computer.run();
+
+console.log(`Part 1&2: ${computer.lastOutput}`);
