e978b76d1d7d9ae6fa44c4423dd254acf8392f37
Milan Fox <milan.fox@diva-e.com>
2024-02-27 10:28:00 +0100

    2022/15 - Beacon Exclusion Zone

--- 2022/day_15.mjs
+++ 2022/day_15.mjs
@@ -1,21 +1,24 @@
-import fs from "fs";
+import fs from 'fs';
 
 const inputData = fs
-  .readFileSync('testInput.txt', 'utf-8')
+  .readFileSync('input.txt', 'utf-8')
   .trim()
   .split('\n')
-  .map(info => info.match(/\d+/g).map(Number))
-  .map(([sensorX, sensorY, beaconX, beaconY]) => ({sensorX, sensorY, beaconX, beaconY}));
+  .map((info) => info.match(/[+-]?\d+/g).map(Number))
+  .map(([ sensorX, sensorY, beaconX, beaconY ]) => ({ sensorX, sensorY, beaconX, beaconY }));
 
-const getBeaconCoverage = ({sensorX, sensorY, beaconX, beaconY}, line) => {
-  const beaconOffsetY = beaconY - sensorY;
-  const beaconOffsetX = beaconX - sensorX;
-  const radius = Math.abs(beaconOffsetX) * 2 + 1 + 2 * beaconOffsetY;
-  const widthAtLine = Math.max(radius - Math.abs(line - sensorY * 2), 0);
-
-  return Array.from({length: widthAtLine}, (_, i) => (sensorX - (Math.floor(widthAtLine / 2)) + i));
+const getBeaconCoverage = ({ sensorX, sensorY, beaconX, beaconY }, line) => {
+  const beaconOffsetY = Math.abs(beaconY - sensorY);
+  const beaconOffsetX = Math.abs(beaconX - sensorX);
+  const radius = beaconOffsetX * 2 + beaconOffsetY * 2 + 1;
+  const widthAtLine = Math.max(radius - Math.abs(line - sensorY) * 2, 0);
+  return Array.from({ length: widthAtLine }, (_, i) => sensorX - Math.floor(widthAtLine / 2) + i);
 };
 
-const blocked = new Set(inputData.map(sensor => getBeaconCoverage(sensor, 10)).flat());
+const getBeaconsInLine = (line) => inputData.filter(({ beaconY }) => beaconY === line);
 
-console.log(`Part 1: ${blocked.size}`);
+const potentiallyBlocked = new Set(inputData.map((sensor) => getBeaconCoverage(sensor, 10)).flat());
+const existingBeaconsInRange = getBeaconsInLine(2000000).map(({ beaconX }) => beaconX);
+const blocked = [ ...potentiallyBlocked ].filter((x) => !existingBeaconsInRange.includes(x));
+
+console.log(`Part 1: ${blocked.length}`);
