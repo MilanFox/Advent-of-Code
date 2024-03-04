c354f2669c3110164ca07e105e93992395ee6360
Milan Fox <milan.fox@diva-e.com>
2024-03-04 21:09:45 +0100

    2022/15 - Beacon Exclusion Zone

--- 2022/day_15.mjs
+++ 2022/day_15.mjs
@@ -5,20 +5,21 @@ const inputData = fs
   .trim()
   .split('\n')
   .map((info) => info.match(/[+-]?\d+/g).map(Number))
-  .map(([ sensorX, sensorY, beaconX, beaconY ]) => ({ sensorX, sensorY, beaconX, beaconY }));
+  .map(([sensorX, sensorY, beaconX, beaconY]) => ({ sensorX, sensorY, beaconX, beaconY }));
 
 const getBeaconCoverage = ({ sensorX, sensorY, beaconX, beaconY }, line) => {
   const beaconOffsetY = Math.abs(beaconY - sensorY);
   const beaconOffsetX = Math.abs(beaconX - sensorX);
-  const radius = beaconOffsetX * 2 + beaconOffsetY * 2 + 1;
-  const widthAtLine = Math.max(radius - Math.abs(line - sensorY) * 2, 0);
-  return Array.from({ length: widthAtLine }, (_, i) => sensorX - Math.floor(widthAtLine / 2) + i);
+  const distanceFromCenter = Math.abs(line - sensorY);
+  const radius = beaconOffsetX + beaconOffsetY;
+  const widthAtLine = 2 * (radius - distanceFromCenter) + 1;
+  return Array.from({ length: widthAtLine }, (_, i) => (sensorX - (radius - distanceFromCenter)) + i);
 };
 
 const getBeaconsInLine = (line) => inputData.filter(({ beaconY }) => beaconY === line);
 
 const potentiallyBlocked = new Set(inputData.map((sensor) => getBeaconCoverage(sensor, 10)).flat());
-const existingBeaconsInRange = getBeaconsInLine(2000000).map(({ beaconX }) => beaconX);
-const blocked = [ ...potentiallyBlocked ].filter((x) => !existingBeaconsInRange.includes(x));
+const existingBeaconsInRange = getBeaconsInLine(20_000).map(({ beaconX }) => beaconX);
+const blocked = [...potentiallyBlocked].filter((x) => !existingBeaconsInRange.includes(x));
 
 console.log(`Part 1: ${blocked.length}`);
