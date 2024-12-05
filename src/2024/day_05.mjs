9153cad304a71978eb60d3265fb209bf068bd203
Milan Fox <milan.fox@linuxmail.org>
2024-12-05 12:02:23 +0100

    24/05 - Print Queue

--- src/2024/day_05.mjs
+++ src/2024/day_05.mjs
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 
 const [_rules, _updates] = fs
-  .readFileSync('input.txt', 'utf-8')
+  .readFileSync('testInput.txt', 'utf-8')
   .trim()
   .split('\n\n').map(data => data.split('\n'));
 
@@ -12,5 +12,32 @@ const isInValidOrder = (update) => rules.every(([a, b]) => update.indexOf(a) < u
 const getMiddleNumber = (update) => update[Math.floor(update.length / 2)];
 const toSum = (acc, cur) => acc + cur;
 
-const checksum = updates.filter(isInValidOrder).map(getMiddleNumber).reduce(toSum, 0);
-console.log(`Part 1: ${checksum}`);
+const correctlySortedUpdatesChecksum = updates
+  .filter(isInValidOrder)
+  .map(getMiddleNumber)
+  .reduce(toSum, 0);
+
+console.log(`Part 1: ${correctlySortedUpdatesChecksum}`);
+
+// https://www.programiz.com/dsa/bubble-sort
+const bubbleSort = (update) => {
+  let swapped;
+  while (swapped !== false) {
+    swapped = false;
+    for (let i = 0; i < update.length - 1; i++) {
+      if (rules.some(([before, after]) => before === update[i] && after === update[i + 1])) {
+        [update[i], update[i + 1]] = [update[i + 1], update[i]];
+        swapped = true;
+      }
+    }
+  }
+  return update.toReversed();
+};
+
+const wronglySortedUpdatesChecksum = updates
+  .filter(update => !isInValidOrder(update))
+  .map(bubbleSort)
+  .map(getMiddleNumber)
+  .reduce(toSum, 0);
+
+console.log(`Part 2: ${wronglySortedUpdatesChecksum}`);
