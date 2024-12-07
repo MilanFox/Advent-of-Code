ecb2433397a1e1cffd6d1f6c9e156cdf5241f5e3
MilanFox <milan.fox@linuxmail.org>
2024-12-07 22:37:05 +0100

    24/03 - Mull it Over

--- src/2024/day_03.mjs
+++ src/2024/day_03.mjs
@@ -1,18 +1,11 @@
 import fs from 'fs';
 
-const inputData = fs.readFileSync('input.txt', 'utf-8');
-
 const multiplication = /mul\((\d+),(\d+)\)/g;
-const anyNumber = /(\d+)/g;
+const extract = (str, rgx) => [...str.matchAll(rgx)].map(match => match.slice(1).map(Number));
+const multiplyAll = (str) => extract(str, multiplication).reduce((acc, [a, b]) => acc + (a * b), 0);
 
-const extract = (str, rgx) => [...str.matchAll(rgx)].map(match => match[0]);
-
-const multiplyAll = (str) => extract(str, multiplication)
-  .map(mult => extract(mult, anyNumber).map(Number))
-  .reduce((acc, [a, b]) => acc + (a * b), 0);
-
+const inputData = fs.readFileSync('input.txt', 'utf-8');
 console.log(`Part 1: ${multiplyAll(inputData)}`);
 
 const enabledParts = inputData.split('do()').map(instr => instr.split('don\'t()')[0]).join('');
-
 console.log(`Part 2: ${multiplyAll(enabledParts)}`);
