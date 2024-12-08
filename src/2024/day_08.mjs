5abbc1def80d487031ce54356afc9525bf2d0769
MilanFox <milan.fox@linuxmail.org>
2024-12-08 07:35:44 +0100

    24/08 - Resonant Collinearity

--- src/2024/day_08.mjs
+++ src/2024/day_08.mjs
@@ -9,26 +9,22 @@ const nodes = map.reduce((count, row, y) => row.reduce((_, cell, x) => {
 
 const isInBounds = ({ x, y }) => y >= 0 && y < map.length && x >= 0 && x < map[0].length;
 
-const getAntiNodes = (nodes, { shouldFindAll } = {}) => {
+const getAntiNodes = (nodes, { startingDistance, maxNodes } = {}) => {
   const antiNodes = new Set([]);
   for (let a = 0; a < nodes.length; a++) {
     for (let b = a + 1; b < nodes.length; b++) {
       const [vectorX, vectorY] = [nodes[b].x - nodes[a].x, nodes[b].y - nodes[a].y];
       const dirs = [{ dir: -1, node: nodes[a] }, { dir: 1, node: nodes[b] }];
       dirs.forEach(({ dir, node }) => {
-        if (shouldFindAll) {
-          let distance = 0;
-          while (true) {
-            const x = node.x + ((vectorX * distance) * dir);
-            const y = node.y + ((vectorY * distance) * dir);
-            if (!isInBounds({ x, y })) break;
-            antiNodes.add(`${x}|${y}`);
-            distance += 1;
-          }
-        } else {
-          const x = node.x + (vectorX * dir);
-          const y = node.y + (vectorY * dir);
-          if (isInBounds({ x, y })) antiNodes.add(`${x}|${y}`);
+        let distance = startingDistance;
+        let count = 0;
+        while (true) {
+          const x = node.x + ((vectorX * distance) * dir);
+          const y = node.y + ((vectorY * distance) * dir);
+          if (!isInBounds({ x, y }) || count >= maxNodes) break;
+          antiNodes.add(`${x}|${y}`);
+          distance += 1;
+          count += 1;
         }
       });
     }
@@ -36,8 +32,10 @@ const getAntiNodes = (nodes, { shouldFindAll } = {}) => {
   return [...antiNodes];
 };
 
-const neighboringAntiNodes = new Set(Object.values(nodes).flatMap(node => getAntiNodes(node)));
+const getAllAntiNodes = (options) => new Set(Object.values(nodes).flatMap(node => getAntiNodes(node, options)));
+
+const neighboringAntiNodes = getAllAntiNodes({ startingDistance: 1, maxNodes: 1 });
 console.log(`Part 1: ${neighboringAntiNodes.size}`);
 
-const allAntiNodes = new Set(Object.values(nodes).flatMap(node => getAntiNodes(node, { shouldFindAll: true })));
-console.log(`Part 1: ${allAntiNodes.size}`);
+const allAntiNodes = getAllAntiNodes({ startingDistance: 0, maxNodes: Infinity });
+console.log(`Part 2: ${allAntiNodes.size}`);
