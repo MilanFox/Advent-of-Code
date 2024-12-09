dfddfc3bf03b40b7fd6662b51172ea6dec7d8269
Milan Fox <milan.fox@linuxmail.org>
2024-12-09 12:17:00 +0100

    24/09 - Disk Fragmenter

--- src/2024/day_09.mjs
+++ src/2024/day_09.mjs
@@ -30,3 +30,59 @@ const fragmentByChunk = () => {
 const checksum = (fragmentationMap) => fragmentationMap.reduce((acc, cur, i) => acc + (cur * i), 0);
 
 console.log(`Part 1: ${checksum(fragmentByChunk())}`);
+
+class File {
+  constructor(length, i) {
+    this.type = 'file';
+    this.length = Number(length);
+    this.id = i / 2;
+  }
+}
+
+class Space {
+  constructor(length) {
+    this.type = 'space';
+    this.length = Number(length);
+  }
+}
+
+const fragmentByFile = () => {
+  const fileSystem = diskMap.map((length, i) => {
+    if (i % 2 === 0) return new File(length, i);
+    else return new Space(length);
+  });
+
+  const files = fileSystem.filter(el => el.type === 'file').toReversed();
+
+  for (const file of files) {
+    const i = fileSystem.findIndex(el => el.type === 'space' && el.length >= file.length);
+    if ((i === -1)) continue;
+    fileSystem.splice(i, 0, file);
+    fileSystem[i + 1].length -= file.length;
+    const lastIndex = fileSystem.findLastIndex(el => el === file);
+
+    if (fileSystem[lastIndex - 1].type === 'space') {
+      if (fileSystem[lastIndex + 1]?.type === 'space') {
+        fileSystem[lastIndex + 1].length += fileSystem[lastIndex - 1].length + file.length;
+        fileSystem.splice(lastIndex - 1, 2);
+        continue;
+      }
+      fileSystem[lastIndex - 1].length += file.length;
+      fileSystem.splice(lastIndex, 1);
+      continue;
+    }
+
+    if (fileSystem[lastIndex + 1]?.type === 'space') {
+      fileSystem[lastIndex + 1].length += file.length;
+      fileSystem.splice(lastIndex, 1);
+      continue;
+    }
+
+    fileSystem.splice(lastIndex, 1, new Space(file.length));
+
+  }
+
+  return fileSystem.flatMap(el => Array(el.length).fill(el.id ?? 0));
+};
+
+console.log(`Part 2: ${checksum(fragmentByFile())}`);
