149a0c6d15de3cfd6c397f1ff53e30d0ca0719e7
Milan Fox <milan.fox@linuxmail.org>
2025-01-07 12:26:57 +0100

    15/05 - Doesn't He Have Intern-Elves For This?

--- src/2015/day_05.mjs
+++ src/2015/day_05.mjs
@@ -1,45 +1,25 @@
 import fs from 'node:fs';
 
-class EvaluationString {
-  constructor(data) {
-    this.string = data;
-    this.chars = [...data];
-  }
+const inputData = fs.readFileSync('input.txt', 'utf-8').trim().split('\n');
 
-  get hasMinimumThreeVowels() {
-    return this.chars.filter(char => [...'aeiou'].includes(char)).length >= 3;
-  }
-
-  get hasDoubleLetter() {
-    return this.chars.some((char, i) => char === this.chars[i + 1]);
-  }
-
-  get avoidsBannedSubstrings() {
-    return ['ab', 'cd', 'pq', 'xy'].every(subString => !this.string.includes(subString));
-  }
-
-  get hasDisjoinedDoubleLetter() {
-    return this.chars.some((char, i) => char === this.chars[i + 2]);
-  }
-
-  get hasRepeatingDoubleLetters() {
-    for (let i = 0; i < this.chars.length - 1; i++) {
-      const subString = this.string.substring(i, i + 2);
-      if (this.string.lastIndexOf(subString) - this.string.indexOf(subString) >= 2) return true;
+const rules = {
+  hasMinimumThreeVowels: (string) => [...string].filter(char => [...'aeiou'].includes(char)).length >= 3,
+  hasDoubleLetter: (string) => [...string].some((char, i) => char === string[i + 1]),
+  avoidsBannedSubstrings: (string) => ['ab', 'cd', 'pq', 'xy'].every(subString => !string.includes(subString)),
+  hasDisjoinedDoubleLetter: (string) => [...string].some((char, i) => char === string[i + 2]),
+  hasRepeatingDoubleLetters: (string) => {
+    for (let i = 0; i < string.length - 1; i++) {
+      const subString = string.substring(i, i + 2);
+      if (string.lastIndexOf(subString) - string.indexOf(subString) >= 2) return true;
     }
     return false;
-  }
+  },
+};
 
-  get isNiceByOldRules() {
-    return [this.hasMinimumThreeVowels, this.hasDoubleLetter, this.avoidsBannedSubstrings].every(val => val === true);
-  }
+const oldRules = [rules.hasMinimumThreeVowels, rules.hasDoubleLetter, rules.avoidsBannedSubstrings];
+const newRules = [rules.hasDisjoinedDoubleLetter, rules.hasRepeatingDoubleLetters];
 
-  get isNiceByNewRules() {
-    return [this.hasRepeatingDoubleLetters, this.hasDisjoinedDoubleLetter].every(val => val === true);
-  }
-}
+const isNice = (string, rules) => rules.every(rule => rule(string));
 
-const inputData = fs.readFileSync('input.txt', 'utf-8').trim().split('\n').map(data => new EvaluationString(data));
-
-console.log(`Part 1: ${inputData.filter(string => string.isNiceByOldRules).length}`);
-console.log(`Part 2: ${inputData.filter(string => string.isNiceByNewRules).length}`);
+console.log(`Part 1: ${inputData.filter(string => isNice(string, oldRules)).length}`);
+console.log(`Part 2: ${inputData.filter(string => isNice(string, newRules)).length}`);
