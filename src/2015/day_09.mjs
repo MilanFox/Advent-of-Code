9dadc9a5995ba5a17a4495d713659c356c10455e
Milan Fox <milan.fox@linuxmail.org>
2025-01-07 22:06:29 +0100

    15/09 - All in a Single Night

---
+++ src/2015/day_09.mjs
@@ -0,0 +1,52 @@
+import fs from 'node:fs';
+
+class Edge {
+  constructor(data) {
+    const [from, _, to, __, weight] = data.split(' ');
+    this.from = from;
+    this.to = to;
+    this.weight = parseInt(weight);
+  }
+
+  get locations() { return [this.from, this.to];}
+}
+
+class Graph {
+  constructor(data) {
+    const edges = data.map(line => new Edge(line));
+    const nodes = [...new Set(edges.flatMap(connection => connection.locations))];
+
+    this.nodes = nodes.reduce((acc, cur) => ({ ...acc, [cur]: {} }), {});
+    this.destinations = nodes;
+
+    edges.forEach(({ from, to, weight }) => {
+      this.nodes[from][to] = weight;
+      this.nodes[to][from] = weight;
+    });
+  }
+
+  getShortestPath() {
+    let shortestPath = Infinity;
+
+    const queue = Object.keys(this.nodes).map(key => [key, [], 0]);
+
+    while (queue.length) {
+      queue.sort((a, b) => a[2] - b[2]);
+      const [currentNode, visited, currentCost] = queue.shift();
+      visited.push(currentNode);
+      const nextDestinations = this.destinations.filter(dest => !visited.includes(dest));
+      if (nextDestinations.length === 0) {
+        shortestPath = Math.min(shortestPath, currentCost);
+        continue;
+      }
+      if (currentCost >= shortestPath) continue;
+      nextDestinations.forEach(dest => queue.push([dest, [...visited], currentCost + this.nodes[currentNode][dest]]));
+    }
+
+    return shortestPath;
+  }
+}
+
+const graph = new Graph(fs.readFileSync('input.txt', 'utf-8').trim().split('\n'));
+
+console.log(`Part 1: ${graph.getShortestPath()}`);
