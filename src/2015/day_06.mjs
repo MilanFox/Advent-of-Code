c1c5f5f00493920f2729e7a31dda816c58c74b41
Milan Fox <milan.fox@linuxmail.org>
2025-01-07 14:38:11 +0100

    15/06 - Probably a Fire Hazard

--- src/2015/day_06.mjs
+++ src/2015/day_06.mjs
@@ -1,23 +1,37 @@
 import fs from 'node:fs';
 
 const instructions = fs.readFileSync('input.txt', 'utf-8').trim().split('\n');
-const lights = Array(1000).fill(0n);
 
-// https://medium.com/@parkerjmed/practical-bit-manipulation-in-javascript-bfd9ef6d6c30
-const operations = {
-  on: (mask, y) => lights[y] |= mask,
-  off: (mask, y) => lights[y] &= ~mask,
-  toggle: (mask, y) => lights[y] ^= mask,
-};
+const binaryLights = Array.from({ length: 1000 }, () => Array(1000).fill(false));
+const dimmableLights = Array.from({ length: 1000 }, () => Array(1000).fill(0));
 
 instructions.forEach(instr => {
   const [_, cmd, start, end] = (/(\w+)\s([\d,]+)\sthrough\s([\d,]+)/g).exec(instr);
   const [xStart, yStart] = start.split(',').map(Number);
   const [xEnd, yEnd] = end.split(',').map(Number);
-  const mask = ((1n << BigInt(xEnd - xStart + 1)) - 1n) << BigInt(xStart);
-  for (let y = yStart; y <= yEnd; y++) operations[cmd](mask, y);
+
+  for (let y = yStart; y <= yEnd; y++) {
+    for (let x = xStart; x <= xEnd; x++) {
+      switch (cmd) {
+        case 'off':
+          binaryLights[y][x] = false;
+          dimmableLights[y][x] = Math.max(0, dimmableLights[y][x] - 1);
+          break;
+        case 'on':
+          binaryLights[y][x] = true;
+          dimmableLights[y][x] += 1;
+          break;
+        case 'toggle':
+          binaryLights[y][x] = !binaryLights[y][x];
+          dimmableLights[y][x] += 2;
+          break;
+      }
+    }
+  }
 });
 
-const numberOfActiveLamps = lights.flatMap(row => [...row.toString(2)].filter(light => light === '1')).length;
-
+const numberOfActiveLamps = binaryLights.flat().filter(Boolean).length;
 console.log(`Part 1: ${numberOfActiveLamps}`);
+
+const totalBrightness = dimmableLights.flat().reduce((acc, cur) => acc + cur, 0);
+console.log(`Part 2: ${totalBrightness}`);
