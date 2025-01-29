46dbc9d3b3e5137fa35c00f97576a1e49e78f58e
Milan Fox <milan.fox@linuxmail.org>
2025-01-29 17:12:17 +0100

    15/18 - Like a GIF For Your Yard

--- src/2015/day_18.mjs
+++ src/2015/day_18.mjs
@@ -2,6 +2,8 @@ import fs from 'node:fs';
 
 let lights = fs.readFileSync('input.txt', 'utf-8').trim().split('\n').map(row => row.split('').map(cell => cell === '#'));
 
+const originalSetup = lights;
+
 const dirs = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
 
 const getNumberOfLitNeighbors = ({ x, y }) => {
@@ -21,9 +23,24 @@ const getNextSnapshot = () => Array.from({ length: lights.length }, (_, y) =>
 
 const getTotalLitLights = () => lights.reduce((total, row) => total + row.reduce((acc, cell) => acc + Number(cell)), 0);
 
-const simulateSteps = (steps) => {
-  for (let i = 0; i < steps; i++) lights = getNextSnapshot();
+const turnOnCornerLights = () => {
+  lights[0][0] = true;
+  lights[0][lights[0].length - 1] = true;
+  lights[lights.length - 1][0] = true;
+  lights[lights.length - 1][lights[0].length - 1] = true;
+};
+
+const simulateSteps = (steps, pinCornerLights) => {
+  for (let i = 0; i < steps; i++) {
+    lights = getNextSnapshot(pinCornerLights);
+    if (pinCornerLights) turnOnCornerLights();
+  }
 };
 
 simulateSteps(100);
+console.log(`Part 1: ${getTotalLitLights()}`);
+
+lights = originalSetup;
+turnOnCornerLights();
+simulateSteps(100, true);
 console.log(`Part 1: ${getTotalLitLights()}`);
