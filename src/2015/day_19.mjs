5df78e85139259235dd7c7b6e050e6d1468e28ca
Milan Fox <milan.fox@linuxmail.org>
2025-01-30 16:02:54 +0100

    15/19 - Medicine for Rudolph

--- src/2015/day_19.mjs
+++ src/2015/day_19.mjs
@@ -5,15 +5,22 @@ class Replacement {
     [this.in, this.out] = data.split(' => ');
   }
 
-  getMoleculesAfterReplacement(molecule) {
-    return molecule
-      .split(this.in)
-      .flatMap((_, i, arr) => i > 0 ? [arr.slice(0, i).join(this.in) + this.out + arr.slice(i).join(this.in)] : []);
+  replaceForward(molecule) {
+    return this.#replace(molecule, this.in, this.out);
+  }
+
+  replaceBackwards(molecule) {
+    return this.#replace(molecule, this.out, this.in);
+  }
+
+  #replace(molecule, from, to) {
+    const parts = molecule.split(from);
+    return parts.slice(1).map((_, i) => parts.slice(0, i + 1).join(from) + to + parts.slice(i + 1).join(from));
   }
 }
 
-const [replacementData, startingPoint] = fs.readFileSync('input.txt', 'utf-8').trim().split('\n\n');
+const [replacementData, testMolecule] = fs.readFileSync('input.txt', 'utf-8').trim().split('\n\n');
 const replacements = replacementData.split('\n').map(data => new Replacement(data));
 
-const moleculesAfterOneReplacement = new Set(replacements.flatMap(replacement => replacement.getMoleculesAfterReplacement(startingPoint)));
+const moleculesAfterOneReplacement = new Set(replacements.flatMap(replacement => replacement.replaceForward(testMolecule)));
 console.log(`Part 1: ${moleculesAfterOneReplacement.size}`);
