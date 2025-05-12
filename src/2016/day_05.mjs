09ad7edb59177f25a6d633748ff5a9e344655a67
Milan Fox <milan.fox@linuxmail.org>
2025-05-12 21:18:46 +0200

    16/05 - How About a Nice Game of Chess

--- src/2016/day_05.mjs
+++ src/2016/day_05.mjs
@@ -3,6 +3,23 @@ import crypto from 'crypto';
 
 const id = readFileSync('input.txt', 'utf-8').trim();
 
+// Progress Bar - Just for flair, since this is puzzle a very expensive calculation by design
+const CLI = {
+  updateLine: (text) => {
+    process.stdout.write(text);
+  },
+  clearLine: () => {
+    process.stdout.clearLine?.(0);
+    process.stdout.cursorTo?.(0);
+  },
+  hideCursor: () => {
+    process.stdout.write('\x1B[?25l');
+  },
+  showCursor: () => {
+    process.stdout.write('\x1B[?25h');
+  },
+};
+
 function* generatePassword() {
   let i = 0;
   while (true) {
@@ -12,9 +29,18 @@ function* generatePassword() {
   }
 }
 
+CLI.hideCursor();
+
 const getPassword = () => {
   const passwordGenerator = generatePassword();
-  return Array.from({ length: 8 }, () => passwordGenerator.next().value[5]).join('');
+
+  const password = Array.from({ length: 8 }, (_, i) => {
+    CLI.updateLine(`\rPart 1: ${'█'.repeat(i)}${'░'.repeat(8 - i)}`);
+    return passwordGenerator.next().value[5];
+  }).join('');
+
+  CLI.clearLine();
+  return password;
 };
 
 console.log(`Part 1: ${getPassword()}`);
@@ -28,11 +54,16 @@ const getStrongPassword = () => {
     const pos = Number(nextHash[5]);
     if (!Number.isNaN(pos) && pos <= 7 && password[pos] === undefined) {
       password[pos] = nextHash[6];
+      const i = password.filter(el => el !== undefined).length;
+      CLI.updateLine(`\rPart 2: ${'█'.repeat(i)}${'░'.repeat(8 - i)}`);
       if (password.every(el => el !== undefined)) break;
     }
   }
 
+  CLI.clearLine();
   return password.join('');
 };
 
 console.log(`Part 2: ${getStrongPassword()}`);
+
+CLI.showCursor();
