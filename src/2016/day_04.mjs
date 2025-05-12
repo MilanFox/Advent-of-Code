1881066ea99548047d82e5bba7de3c8b150a1b7f
Milan Fox <milan.fox@linuxmail.org>
2025-05-12 16:13:09 +0200

    16/04 - Security Through Obscurity

--- src/2016/day_04.mjs
+++ src/2016/day_04.mjs
@@ -23,9 +23,20 @@ class Room {
     const realMostCommonLetters = this.letterCount.slice(0, 5).map(([char]) => char).join('');
     return realMostCommonLetters === this.mostCommonLetters;
   }
+
+  get decryptedName() {
+    const decryptedBlocks = this.encryptedName.map(block => {
+      const letters = [...block].map(c => c.charCodeAt(0) - 97);
+      return letters.map(code => String.fromCharCode(((code + this.sectorID) % 26) + 97)).join('');
+    });
+    return decryptedBlocks.join(' ');
+  }
 }
 
 const rooms = readFileSync('input.txt', 'utf-8').trim().split('\n').map(data => new Room(data));
 
 const realRooms = rooms.filter(room => room.isReal);
 console.log(`Part 1: ${realRooms.map(room => room.sectorID).reduce((acc, cur) => acc + cur, 0)}`);
+
+const northPoleObjectStorage = realRooms.find(room => room.decryptedName === 'northpole object storage');
+console.log(`Part 2: ${northPoleObjectStorage.sectorID}`);
