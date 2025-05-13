e4615dcd10e7410869773affef49c784bcc853ce
Milan Fox <milan.fox@linuxmail.org>
2025-05-13 15:00:08 +0200

    16/09 - Explosives in Cyberspace

--- src/2016/day_09.mjs
+++ src/2016/day_09.mjs
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 
 const compressedData = readFileSync('input.txt', 'utf-8').trim();
 
-const decompress = (sequence, { version } = { version: 1 }) => {
+const getDecompressedSize = (sequence, { version } = { version: 1 }) => {
   let decompressedDataSize = 0;
 
   for (let i = 0; i < sequence.length; i++) {
@@ -12,7 +12,7 @@ const decompress = (sequence, { version } = { version: 1 }) => {
       const remainingString = sequence.substring(i, sequence.length);
       const [match, length, repeatCount] = remainingString.match(/^\D*\((\d+)x(\d+)\)/);
       let repeatStringSize = remainingString.substring(match.length, match.length + Number(length)).length;
-      if (version === 2) repeatStringSize = decompress(remainingString.substring(match.length, match.length + Number(length)), { version });
+      if (version === 2) repeatStringSize = getDecompressedSize(remainingString.substring(match.length, match.length + Number(length)), { version });
       for (let i = 0; i < repeatCount; i++) decompressedDataSize += repeatStringSize;
       i += match.length + Number(length) - 1;
     }
@@ -22,5 +22,5 @@ const decompress = (sequence, { version } = { version: 1 }) => {
   return decompressedDataSize;
 };
 
-console.log(`Part 1: ${decompress(compressedData)}`);
-console.log(`Part 2: ${decompress(compressedData, { version: 2 })}`);
+console.log(`Part 1: ${getDecompressedSize(compressedData)}`);
+console.log(`Part 2: ${getDecompressedSize(compressedData, { version: 2 })}`);
