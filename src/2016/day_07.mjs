9cf3918e26521b8b2160ea30ed8b3b272decd8a3
Milan Fox <milan.fox@linuxmail.org>
2025-05-13 09:50:16 +0200

    16/07 - Internet Protocol Version 7

--- src/2016/day_07.mjs
+++ src/2016/day_07.mjs
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 
 class IPv7 {
   constructor(data) {
-    this.regularSequences = data.match(/^[a-z]+|[a-z]+$|(?<=])[a-z]+(?=\[)/g);
+    this.supernetSequences = data.match(/^[a-z]+|[a-z]+$|(?<=])[a-z]+(?=\[)/g);
     this.hypernetSequences = data.match(/(?<=\[)[a-z]+(?=])/g);
   }
 
@@ -22,12 +22,27 @@ class IPv7 {
       }
       return hasBypass;
     };
-    
-    return this.regularSequences.some(hasBypass) && !this.hypernetSequences.some(hasBypass);
+
+    return this.supernetSequences.some(hasBypass) && !this.hypernetSequences.some(hasBypass);
+  }
+
+  get supportsSSL() {
+    const abaSequences = Array.from(new Set(this.supernetSequences.flatMap(seq => {
+      const foundSequences = new Set();
+      for (let i = 0; i < seq.length - 2; i++) {
+        if (seq[i] === seq[i + 2] && seq[i] !== seq[i + 1]) foundSequences.add(seq.substring(i, i + 3));
+      }
+      return Array.from(foundSequences);
+    })));
+
+    return abaSequences.some(aba => {
+      const bab = `${aba[1]}${aba[0]}${aba[1]}`;
+      return this.hypernetSequences.some(seq => seq.includes(bab));
+    });
   }
 }
 
 const ips = readFileSync('input.txt', 'utf-8').trim().split('\n').map(data => new IPv7(data));
 
-const vulnerableIPs = ips.filter(ip => ip.supportsTLS);
-console.log(`Part 1: ${vulnerableIPs.length}`);
+console.log(`Part 1: ${ips.filter(ip => ip.supportsTLS).length}`);
+console.log(`Part 2: ${ips.filter(ip => ip.supportsSSL).length}`);
