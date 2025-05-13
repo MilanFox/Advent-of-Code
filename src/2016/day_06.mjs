af21f5893dd04e97b2a935aab836813753367ce8
Milan Fox <milan.fox@linuxmail.org>
2025-05-13 08:31:14 +0200

    16/06 - Signals and Noise

--- src/2016/day_06.mjs
+++ src/2016/day_06.mjs
@@ -2,16 +2,22 @@ import { readFileSync } from 'node:fs';
 
 const inputData = readFileSync('input.txt', 'utf-8').trim().split('\n');
 
-const decypherSignal = () => {
+const decypherSignal = ({ method }) => {
   const columns = Array.from({ length: inputData[0].length }, (_, i) => inputData.map(line => line[i]));
 
   const repeatedLetters = columns.map(col => {
     const count = {};
     col.forEach(el => count[el] = (count[el] || 0) + 1);
-    return Object.entries(count).toSorted((a, b) => b[1] - a[1]).at(0).at(0);
+
+    const letterCount = Object.entries(count);
+    if (method === 'mostCommon') letterCount.sort((a, b) => b[1] - a[1]);
+    if (method === 'leastCommon') letterCount.sort((a, b) => a[1] - b[1]);
+
+    return letterCount.at(0).at(0);
   });
 
   return repeatedLetters.join('');
 };
 
-console.log(`Part 1: ${decypherSignal()}`);
+console.log(`Part 1: ${decypherSignal({ method: 'mostCommon' })}`);
+console.log(`Part 2: ${decypherSignal({ method: 'leastCommon' })}`);
