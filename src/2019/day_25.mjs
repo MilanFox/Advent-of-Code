8d288256fb0a77221cb715755c3ed14be5121fd2
Milan Fox <milan.fox@linuxmail.org>
2025-07-07 18:57:53 +0200

    19/25 - Cryostasis

--- src/2019/day_25.mjs
+++ src/2019/day_25.mjs
@@ -4,12 +4,12 @@ import { getUserInput } from './utils/getUserInput.mjs';
 
 const memory = fs.readFileSync('testInput.txt', 'utf-8').trim().split(',').map(Number);
 
-const vm1 = new IntCodeComputer(memory, { logOutputAsAscii: true });
+const vm = new IntCodeComputer(memory, { logOutputAsAscii: true });
 
-vm1.on(vm1.EVENT_NAMES.NEEDS_INPUT, async () => {
+vm.on(vm.EVENT_NAMES.NEEDS_INPUT, async () => {
   const userInput = await getUserInput({ isAscii: true });
-  for (const asciiCode of userInput) vm1.queueInput(asciiCode);
-  await vm1.run();
+  for (const asciiCode of userInput) vm.queueInput(asciiCode);
+  await vm.run();
 });
 
-await vm1.run();
+await vm.run();
