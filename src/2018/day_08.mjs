96baa975fef82f3824060b5efe79d39ba2f80329
Milan Fox <milan.fox@linuxmail.org>
2025-07-11 17:15:53 +0200

    18/05 - Memory Maneuver

--- src/2018/day_08.mjs
+++ src/2018/day_08.mjs
@@ -22,9 +22,15 @@ class TreeNode {
   get checksum() {
     return this.metadata.reduce((acc, cur) => acc + cur, 0) + this.children.reduce((acc, cur) => acc + cur.checksum, 0);
   }
+
+  get value() {
+    if (!this.children.length) return this.metadata.reduce((acc, cur) => acc + cur, 0);
+    else return this.metadata.reduce((acc, i) => acc + (this.children[i - 1]?.value || 0), 0);
+  }
 }
 
 const metadata = readFileSync('input.txt', 'utf-8').trim().split(' ').map(Number);
 const navigationSystem = new Tree(metadata);
 
 console.log(`Part 1: ${navigationSystem.rootNode.checksum}`);
+console.log(`Part 2: ${navigationSystem.rootNode.value}`);
