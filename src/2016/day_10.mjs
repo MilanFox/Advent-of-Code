7606c1f448106ec9718c476fc9c3743b98ed0b23
Milan Fox <milan.fox@linuxmail.org>
2025-07-30 15:34:06 +0200

    16/10 - Balance Bots

---
+++ src/2016/day_10.mjs
@@ -0,0 +1,74 @@
+import { readFileSync } from 'node:fs';
+
+class Output {
+  constructor(id) {
+    this.id = id;
+  }
+
+  values = new Set();
+}
+
+class Bot {
+  constructor(id, value) {
+    this.id = Number(id);
+    if (value !== undefined) this.values.add(value);
+  }
+
+  values = new Set();
+  targetLow = null;
+  targetHigh = null;
+
+  handOverMicrochips() {
+    const [low, high] = [...this.values].toSorted((a, b) => a - b);
+    this.targetLow.values.add(low);
+    this.targetHigh.values.add(high);
+    this.values.clear();
+  }
+}
+
+class Factory {
+  constructor(data) {
+    data.forEach(instr => {
+      if (instr.startsWith('value')) {
+        const parser = /value (?<value>\d+) goes to bot (?<id>\d+)/;
+        const { value, id } = parser.exec(instr).groups;
+        if (!this.bots[id]) this.bots[id] = (new Bot(id, Number(value)));
+        else (this.bots[id].values.add(Number(value)));
+      }
+      if (instr.startsWith('bot')) {
+        const parser = /bot (?<id>\d+) gives low to (?<low>\w+ \d+) and high to (?<high>\w+ \d+)/;
+        const { id, low, high } = parser.exec(instr).groups;
+        if (!this.bots[id]) this.bots[id] = (new Bot(id));
+        this.bots[id].targetLow = low;
+        this.bots[id].targetHigh = high;
+      }
+    });
+
+    const getElement = (name) => {
+      const [type, id] = name.split(' ');
+      if (type === 'bot') return this.bots[id];
+      if (!this.outputs[id]) this.outputs[id] = new Output(Number(id));
+      return this.outputs[id];
+    };
+
+    Object.values(this.bots).forEach(bot => {
+      bot.targetLow = getElement(bot.targetLow);
+      bot.targetHigh = getElement(bot.targetHigh);
+    });
+  }
+
+  bots = {};
+  outputs = {};
+
+  run({ logAt }) {
+    while (true) {
+      const nextBot = Object.values(this.bots).find(bot => bot.values.size === 2);
+      if (!nextBot) break;
+      if (logAt.every(chipId => nextBot.values.has(chipId))) console.log(`Part 1: ${nextBot.id}`);
+      nextBot.handOverMicrochips();
+    }
+  }
+}
+
+const factory = new Factory(readFileSync('input.txt', 'utf-8').trim().split('\n'));
+factory.run({ logAt: [61, 17] });
