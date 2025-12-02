c13fbc191d3c2f5fec1ed414c93a6f5b540b2cd3
Milan Fox <milan.fox@linuxmail.org>
2025-12-02 20:54:35 +0100

    25/02 - Gift Shop

--- src/2025/day_02.mjs
+++ src/2025/day_02.mjs
@@ -20,3 +20,18 @@ const twoWaySymmetryChecksum = inputData.reduce((acc, [start, end]) => {
 }, 0);
 
 console.log(`Part 1: ${twoWaySymmetryChecksum}`);
+
+const nWaySymmetryChecksum = inputData.reduce((acc, [start, end]) => {
+  search: for (let i = start; i <= end; i++) {
+    const num = String(i);
+    for (let j = 1; j <= num.length / 2; j++) {
+      if (num.length % j === 0 && isInvalid(num, j)) {
+        acc += i;
+        continue search;
+      }
+    }
+  }
+  return acc;
+}, 0);
+
+console.log(`Part 2: ${nWaySymmetryChecksum}`);
