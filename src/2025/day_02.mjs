b9ce4396b267304489e3211de4a8099689ccc46d
Milan Fox <milan.fox@linuxmail.org>
2025-12-02 09:34:48 +0100

    25/02 - Gift Shop

--- src/2025/day_02.mjs
+++ src/2025/day_02.mjs
@@ -1,28 +1,21 @@
 import { readFileSync } from 'node:fs';
 
-const inputData = readFileSync('input.txt', 'utf-8').trim().split(',').map(line => line.split('-'));
-
-const clampToEvenNumbers = (range) => {
-  let [start, end] = range;
-  if (start.length % 2 !== 0) start = 10 ** start.length;
-  if (end.length % 2 !== 0) end = 10 ** (end.length - 1) - 1;
-  return [start, end].map(Number);
-};
+const inputData = readFileSync('testinput.txt', 'utf-8').trim().split(',').map(line => line.split('-'));
 
 const findInvalidIDsInRange = (range) => {
-  const [start, end] = clampToEvenNumbers(range);
+  const [start, end] = range.map(Number);
 
   let checksum = 0;
 
   let i = start;
 
   search: while (true) {
-    i++;
     if (i > end) break;
-
     const curNumber = String(i);
     let a = 0;
     let b = curNumber.length / 2;
+
+    i++;
 
     for (let j = 0; j < curNumber.length / 2; j++) {
       if (curNumber[a + j] !== curNumber[b + j]) continue search;
