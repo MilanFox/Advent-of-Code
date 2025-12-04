a64cbb93b1b9b8bf5b529cfc82b87d6584222cf7
Milan Fox <milan.fox@linuxmail.org>
2025-12-04 09:39:51 +0100

    25/04 - Printing Department

--- src/2025/day_04.mjs
+++ src/2025/day_04.mjs
@@ -6,18 +6,21 @@ const PAPER_ROLL = '@';
 
 const dirs = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]];
 
-const isReachable = ({ x, y }) =>
-  dirs.reduce((acc, [dx, dy]) => (floorMap[y + dy]?.[x + dx] !== PAPER_ROLL) ? acc : (acc + 1), 0) <= 3;
+const isReachable = ({ x, y }) => {
+  const numberOfSurroundingRolls = dirs.reduce((acc, [dx, dy]) => {
+    if (floorMap[y + dy]?.[x + dx] !== PAPER_ROLL) return acc;
+    return acc + 1;
+  }, 0);
+  return numberOfSurroundingRolls <= 3;
+};
 
 const getReachablePaperRolls = () => {
   const rolls = [];
-
   for (let y = 0; y < floorMap.length; y++) {
     for (let x = 0; x < floorMap[0].length; x++) {
       if (floorMap[y][x] === PAPER_ROLL && isReachable({ x, y })) rolls.push([x, y]);
     }
   }
-
   return rolls;
 };
 
@@ -28,9 +31,7 @@ let totalRemovableRolls = 0;
 while (true) {
   const removableRolls = getReachablePaperRolls();
   if (!removableRolls.length) break;
-
   totalRemovableRolls += removableRolls.length;
-
   removableRolls.forEach(([x, y]) => floorMap[y][x] = '.');
 }
 
