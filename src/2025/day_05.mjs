ff09f9bd8e9c8c370ecce039b9681f0145f56084
Milan Fox <milan.fox@linuxmail.org>
2025-12-05 11:04:31 +0100

    25/05 - Cafeteria

--- src/2025/day_05.mjs
+++ src/2025/day_05.mjs
@@ -2,11 +2,11 @@ import { readFileSync } from 'node:fs';
 
 const [rangeData, ingredientData] = readFileSync('input.txt', 'utf-8').trim().split('\n\n');
 
-const atNewLine = '\n'
+const atNewLine = '\n';
 
-const toRanges = line => line.split('-').map(Number)
+const toRanges = line => line.split('-').map(Number);
 
-const byStartAndEnd = ([startA, endA], [startB, endB]) => startA - startB || endA - endB
+const byStartAndEnd = ([startA, endA], [startB, endB]) => startA - startB || endA - endB;
 
 const toDeduplicatedRanges = (acc, [start, end]) => {
   const last = acc.at(-1);
@@ -14,7 +14,7 @@ const toDeduplicatedRanges = (acc, [start, end]) => {
   else if (last[1] < start) acc.push([start, end]);
   else if (last[1] < end) last[1] = end;
   return acc;
-}
+};
 
 const freshRanges = rangeData.split(atNewLine).map(toRanges).sort(byStartAndEnd).reduce(toDeduplicatedRanges, []);
 const ingredients = ingredientData.split(atNewLine).map(Number);
