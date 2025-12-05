7340c0ba30b75dcfd8ff4ad5c423e7843eca33b2
Milan Fox <milan.fox@linuxmail.org>
2025-12-05 10:28:34 +0100

    25/05 - Cafeteria

--- src/2025/day_05.mjs
+++ src/2025/day_05.mjs
@@ -2,18 +2,19 @@ import { readFileSync } from 'node:fs';
 
 const [rangeData, ingredientData] = readFileSync('input.txt', 'utf-8').trim().split('\n\n');
 
-const freshRanges = rangeData
-  .split('\n')
-  .map(line => line.split('-').map(Number))
-  .toSorted((([startA, endA], [startB, endB]) => startA - startB || endA - endB))
-  .reduce((acc, [start, end]) => {
-    const last = acc.at(-1);
-    if (!last) acc.push([start, end]);
-    else if (last[1] < start) acc.push([start, end]);
-    else if (last[1] < end) last[1] = end;
-    return acc;
-  }, []);
+const toRanges = line => line.split('-').map(Number)
 
+const byStartAndEnd = ([startA, endA], [startB, endB]) => startA - startB || endA - endB
+
+const toDeduplicatedRanges = (acc, [start, end]) => {
+  const last = acc.at(-1);
+  if (!last) acc.push([start, end]);
+  else if (last[1] < start) acc.push([start, end]);
+  else if (last[1] < end) last[1] = end;
+  return acc;
+}
+
+const freshRanges = rangeData.split('\n').map(toRanges).sort(byStartAndEnd).reduce(toDeduplicatedRanges, []);
 const ingredients = ingredientData.split('\n').map(Number);
 
 const isFresh = id => freshRanges.some(([start, end]) => id >= start && id <= end);
