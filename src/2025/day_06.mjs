9ddfc156af126a5849e1838a116570a8477bca80
Milan Fox <milan.fox@linuxmail.org>
2025-12-06 10:52:27 +0100

    2025/06 - Trash Compactor

--- src/2025/day_06.mjs
+++ src/2025/day_06.mjs
@@ -1,14 +1,23 @@
 import { readFileSync } from 'node:fs';
 
-const inputData = readFileSync('input.txt', 'utf-8').trim().split('\n').map(line => line.trim().split(/\s+/));
-const operator = inputData.pop();
+const rows = readFileSync('testinput.txt', 'utf-8').trim().split('\n');
+const type1data = rows.map(line => line.trim().split(/\s+/));
+const operators = type1data.pop();
 
-const grandTotal = inputData[0].reduce((total, first, i) =>
-    total + inputData.reduce((acc, cur, j) => {
-            if (j === 0) return acc;
-            if (operator[i] === '+') return acc + Number(cur[i]);
-            if (operator[i] === '*') return acc * Number(cur[i]);
-          }, Number(first)),
-  0);
+const dataCols = type1data[0].reduce((total, _, j) => {
+  total.push(
+    type1data.reduce((acc, cur) => {
+      acc.values.push(cur[j]);
+      return acc;
+    }, { values: [], operator: operators[j] }),
+  );
+  return total;
+}, []);
 
-console.log(`Part 1: ${grandTotal}`);
+const calculate = ({ values, operator }) => values.reduce((acc, cur, i) => {
+  if (i === 0) return acc;
+  if (operator === '+') return acc + Number(cur);
+  if (operator === '*') return acc * Number(cur);
+}, Number(values[0]));
+
+console.log(`Part 1: ${dataCols.reduce((acc, cur) => acc + calculate(cur), 0)}`);
