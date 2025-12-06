4d30274446fab0d912b7ec140937bae725c8819a
Milan Fox <milan.fox@linuxmail.org>
2025-12-06 11:19:55 +0100

    2025/06 - Trash Compactor

--- src/2025/day_06.mjs
+++ src/2025/day_06.mjs
@@ -1,6 +1,6 @@
 import { readFileSync } from 'node:fs';
 
-const rows = readFileSync('testinput.txt', 'utf-8').trim().split('\n');
+const rows = readFileSync('input.txt', 'utf-8').trim().split('\n');
 const type1data = rows.map(line => line.trim().split(/\s+/));
 const operators = type1data.pop();
 
@@ -21,3 +21,14 @@ const calculate = ({ values, operator }) => values.reduce((acc, cur, i) => {
 }, Number(values[0]));
 
 console.log(`Part 1: ${dataCols.reduce((acc, cur) => acc + calculate(cur), 0)}`);
+
+const type2Data = Array
+  .from({ length: rows.reduce((acc, cur) => Math.max(acc, cur.length), 0) }, (_, i) => rows.slice(0, -1).reduce((acc, row) => acc + (row[i] ?? ''), ''))
+  .reduce((acc, cur) => {
+    if (cur.match(/^\s+$/)) acc.push([]);
+    else acc.at(-1).push(Number(cur));
+    return acc;
+  }, [[]])
+  .map((values, i) => ({ values, operator: operators[i] }));
+
+console.log(`Part 2: ${type2Data.reduce((acc, cur) => acc + calculate(cur), 0)}`);
