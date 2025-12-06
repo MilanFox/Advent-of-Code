444e99aa84b76af664db3de36a0248acab2997e6
Milan Fox <milan.fox@linuxmail.org>
2025-12-06 11:29:01 +0100

    2025/06 - Trash Compactor

--- src/2025/day_06.mjs
+++ src/2025/day_06.mjs
@@ -4,31 +4,23 @@ const rows = readFileSync('input.txt', 'utf-8').trim().split('\n');
 const type1data = rows.map(line => line.trim().split(/\s+/));
 const operators = type1data.pop();
 
-const dataCols = type1data[0].reduce((total, _, j) => {
-  total.push(
-    type1data.reduce((acc, cur) => {
-      acc.values.push(cur[j]);
-      return acc;
-    }, { values: [], operator: operators[j] }),
-  );
-  return total;
-}, []);
+const dataCols = type1data[0].map((_, j) => ({
+  values: type1data.map(row => row[j]),
+  operator: operators[j],
+}));
 
-const calculate = ({ values, operator }) => values.reduce((acc, cur, i) => {
-  if (i === 0) return acc;
-  if (operator === '+') return acc + Number(cur);
-  if (operator === '*') return acc * Number(cur);
-}, Number(values[0]));
+const calculate = ({ values, operator }) => values
+  .slice(1)
+  .reduce((acc, cur) => {
+    if (operator === '+') return acc + Number(cur);
+    if (operator === '*') return acc * Number(cur);
+  }, Number(values[0]));
 
 console.log(`Part 1: ${dataCols.reduce((acc, cur) => acc + calculate(cur), 0)}`);
 
 const type2Data = Array
-  .from({ length: rows.reduce((acc, cur) => Math.max(acc, cur.length), 0) }, (_, i) => rows.slice(0, -1).reduce((acc, row) => acc + (row[i] ?? ''), ''))
-  .reduce((acc, cur) => {
-    if (cur.match(/^\s+$/)) acc.push([]);
-    else acc.at(-1).push(Number(cur));
-    return acc;
-  }, [[]])
+  .from({ length: Math.max(...rows.map(r => r.length)) }, (_, i) => rows.slice(0, -1).map(r => r[i] ?? '').join(''))
+  .reduce((acc, cur) => acc.concat(/^\s+$/.test(cur) ? [[]] : [[Number(cur)]].map(v => acc.pop()?.concat(v) ?? v)), [])
   .map((values, i) => ({ values, operator: operators[i] }));
 
 console.log(`Part 2: ${type2Data.reduce((acc, cur) => acc + calculate(cur), 0)}`);
