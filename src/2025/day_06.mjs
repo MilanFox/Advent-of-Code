9995817df9156a8a8d5838592bccf6b342825ddb
Milan Fox <milan.fox@linuxmail.org>
2025-12-08 16:41:07 +0100

    25/06 - Trash Compactor

--- src/2025/day_06.mjs
+++ src/2025/day_06.mjs
@@ -1,24 +1,21 @@
 import { readFileSync } from 'node:fs';
 
-const rows = readFileSync('input.txt', 'utf-8').trim().split('\n');
-const type1data = rows.map(line => line.trim().split(/\s+/));
-const operators = type1data.pop();
+const inputLines = readFileSync('input.txt', 'utf-8').trim().split('\n');
+const gridRows = inputLines.map(line => line.trim().split(/\s+/));
+const operators = gridRows.pop();
 
-const dataCols = type1data[0]
-  .map((_, j) => ({ values: type1data.map(row => row[j]), operator: operators[j] }));
+const columns = gridRows[0]
+  .map((_, j) => ({ values: gridRows.map(row => row[j]), operator: operators[j] }));
 
-const calculate = ({ values, operator }) => values
+const toResult = (total, { values, operator }) => total + values
   .slice(1)
-  .reduce((acc, cur) => {
-    if (operator === '+') return acc + Number(cur);
-    if (operator === '*') return acc * Number(cur);
-  }, Number(values[0]));
+  .reduce((acc, cur) => operator === '+' ? acc + Number(cur) : acc * Number(cur), Number(values[0]));
 
-console.log(`Part 1: ${dataCols.reduce((acc, cur) => acc + calculate(cur), 0)}`);
+console.log(`Part 1: ${columns.reduce(toResult, 0)}`);
 
-const type2Data = Array
-  .from({ length: Math.max(...rows.map(r => r.length)) }, (_, i) => rows.slice(0, -1).map(r => r[i] ?? '').join(''))
+const decodedColumns = Array
+  .from({ length: Math.max(...inputLines.map(r => r.length)) }, (_, i) => inputLines.slice(0, -1).map(r => r[i] ?? '').join(''))
   .reduce((acc, cur) => acc.concat(/^\s+$/.test(cur) ? [[]] : [[Number(cur)]].map(v => acc.pop()?.concat(v) ?? v)), [])
   .map((values, i) => ({ values, operator: operators[i] }));
 
-console.log(`Part 2: ${type2Data.reduce((acc, cur) => acc + calculate(cur), 0)}`);
+console.log(`Part 2: ${decodedColumns.reduce(toResult, 0)}`);
