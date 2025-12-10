044223b992b080941897e6a0f0b6e1b618848624
Milan Fox <milan.fox@linuxmail.org>
2025-12-10 11:06:50 +0100

    25/10 - Factory

---
+++ src/2025/day_10.mjs
@@ -0,0 +1,34 @@
+import { readFileSync } from 'node:fs';
+
+const inputData = readFileSync('input.txt', 'utf-8').trim().split('\n').map(line => {
+  const indicatorLights = line.match(/(?<=\[).*?(?=])/)[0].split('').map(char => char === '#' ? 1 : 0).join('');
+  const numLights = indicatorLights.length;
+  const indicatorLightValue = parseInt(indicatorLights, 2);
+
+  const buttonValueMasks = [...line.matchAll(/(?<=\().*?(?=\))/g)].map(m => m[0].split(',').reduce((acc, i) => acc | (1 << (numLights - 1 - i)), 0));
+
+  return { indicatorLightValue, buttonValueMasks };
+});
+
+const findQuickestPath = ({ indicatorLightValue, buttonValueMasks }) => {
+  const queue = [[0, 0]];
+  const seen = new Set([0]);
+
+  while (queue.length) {
+    const [cur, presses] = queue.shift();
+
+    if (cur === indicatorLightValue) return presses;
+
+    for (const mask of buttonValueMasks) {
+      const next = cur ^ mask;
+
+      if (!seen.has(next)) {
+        seen.add(next);
+        queue.push([next, presses + 1]);
+      }
+    }
+  }
+};
+
+const fewestButtonPresses = inputData.map(findQuickestPath).reduce((acc, cur) => acc + cur, 0);
+console.log(`Part 1: ${fewestButtonPresses}`);
