34a423a178e6ee3b40d1ad3244ee06f107c51e5e
Milan Fox <milan.fox@linuxmail.org>
2025-12-10 20:53:34 +0100

    25/10 - Factory

--- src/2025/day_10.mjs
+++ src/2025/day_10.mjs
@@ -4,13 +4,9 @@ const inputData = readFileSync('input.txt', 'utf-8').trim().split('\n').map(line
   const indicatorLights = line.match(/(?<=\[).*?(?=])/)[0].split('').map(char => char === '#' ? 1 : 0).join('');
   const numLights = indicatorLights.length;
   const indicatorLightValue = parseInt(indicatorLights, 2);
-
   const buttons = [...line.matchAll(/(?<=\().*?(?=\))/g)].map(m => m[0].split(',').map(Number));
   const buttonValueMasks = buttons.map(button => button.reduce((acc, i) => acc | (1 << (numLights - 1 - i)), 0));
-
-  const joltageTarget = line.match(/(?<=\{).*?(?=})/)[0].split(',').map(Number);
-
-  return { indicatorLightValue, buttonValueMasks, joltageTarget, buttons };
+  return { indicatorLightValue, buttonValueMasks };
 });
 
 const findFastestStartupSequence = ({ indicatorLightValue, buttonValueMasks }) => {
@@ -19,12 +15,9 @@ const findFastestStartupSequence = ({ indicatorLightValue, buttonValueMasks }) =
 
   while (queue.length) {
     const [cur, presses] = queue.shift();
-
     if (cur === indicatorLightValue) return presses;
-
     for (const mask of buttonValueMasks) {
       const next = cur ^ mask;
-
       if (!seen.has(next)) {
         seen.add(next);
         queue.push([next, presses + 1]);
