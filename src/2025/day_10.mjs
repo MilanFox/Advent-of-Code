83151c5d3215c12b158d2ea3d6e0acd6b51fbdb2
Milan Fox <milan.fox@linuxmail.org>
2025-12-10 13:08:40 +0100

    25/10 - Factory

--- src/2025/day_10.mjs
+++ src/2025/day_10.mjs
@@ -36,12 +36,62 @@ const findFastestStartupSequence = ({ indicatorLightValue, buttonValueMasks }) =
 const startupTime = inputData.map(findFastestStartupSequence).reduce((acc, cur) => acc + cur, 0);
 console.log(`Part 1: ${startupTime}`);
 
+class PriorityQueue {
+  #heap = [];
+
+  push(obj) {
+    this.#heap.push(obj);
+    this.#bubbleUp();
+  }
+
+  pop() {
+    if (!this.#heap.length) return null;
+    const min = this.#heap[0];
+    const end = this.#heap.pop();
+    if (this.#heap.length > 0) {
+      this.#heap[0] = end;
+      this.#bubbleDown();
+    }
+    return min;
+  }
+
+  #bubbleUp() {
+    let currentIndex = this.#heap.length - 1;
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      if (this.#heap[currentIndex].priority >= this.#heap[parentIndex].priority) break;
+      [this.#heap[currentIndex], this.#heap[parentIndex]] = [this.#heap[parentIndex], this.#heap[currentIndex]];
+      currentIndex = parentIndex;
+    }
+  }
+
+  #bubbleDown() {
+    let currentIndex = 0;
+    const heapSize = this.#heap.length;
+    while (true) {
+      const leftIndex = 2 * currentIndex + 1;
+      const rightIndex = 2 * currentIndex + 2;
+      let smallest = currentIndex;
+      if (leftIndex < heapSize && this.#heap[leftIndex].priority < this.#heap[smallest].priority) smallest = leftIndex;
+      if (rightIndex < heapSize && this.#heap[rightIndex].priority < this.#heap[smallest].priority) smallest = rightIndex;
+      if (smallest === currentIndex) break;
+      [this.#heap[currentIndex], this.#heap[smallest]] = [this.#heap[smallest], this.#heap[currentIndex]];
+      currentIndex = smallest;
+    }
+  }
+}
+
 const findFastestConfiguration = ({ joltageTarget, buttons }) => {
-  const queue = [[Array(joltageTarget.length).fill(0), 0]];
+  const queue = new PriorityQueue();
+  const getPriorityScore = (state) => state.reduce((acc, cur, i) => acc + joltageTarget[i] - cur, 0);
+  const startState = Array(joltageTarget.length).fill(0);
+
+  queue.push({ value: [startState, 0], priority: getPriorityScore(startState) });
+
   const seen = new Set([Array(joltageTarget.length).fill(0).join('-')]);
 
-  while (queue.length) {
-    const [cur, presses] = queue.shift();
+  while (true) {
+    const { value: [cur, presses] } = queue.pop();
 
     if (cur.every((num, i) => num === joltageTarget[i])) return presses;
 
@@ -49,9 +99,11 @@ const findFastestConfiguration = ({ joltageTarget, buttons }) => {
       const next = [...cur];
       for (const i of button) next[i] += 1;
 
+      if (next.some((value, i) => value > joltageTarget[i])) continue;
+
       if (!seen.has(next.join('-'))) {
         seen.add(next.join('-'));
-        queue.push([next, presses + 1]);
+        queue.push({ value: [next, presses + 1], priority: getPriorityScore(next) });
       }
     }
   }
