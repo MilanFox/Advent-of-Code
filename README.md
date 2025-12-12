10cf660afba6c4c5f3052af0866e9946d246a1d8
Milan Fox <milan.fox@linuxmail.org>
2025-12-12 09:26:29 +0100

    25/12 - Christmas Tree Farm

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ noise?_"; but **not** "_Generate a function that does X_",  "_Refactor to do X_"
 
 ![Advent of Code](assets/divider.png)
 
-## 🎀 [2025](https://adventofcode.com/2025) ![](https://progress-bar.xyz/21?scale=23&suffix=/23)
+## 🎀 [2025](https://adventofcode.com/2025) ![](https://progress-bar.xyz/22?scale=23&suffix=/23)
 
 [![](https://img.shields.io/badge/Day_01-★★-gold)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_01.mjs)
 [![](https://img.shields.io/badge/Day_02-★★-gold)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_02.mjs)
@@ -66,7 +66,7 @@ noise?_"; but **not** "_Generate a function that does X_",  "_Refactor to do X_"
 [![](https://img.shields.io/badge/Day_09-★★-gold)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_09.mjs)
 [![](https://img.shields.io/badge/Day_10-★☆-silver)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_10.mjs)
 [![](https://img.shields.io/badge/Day_11-★★-gold)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_11.mjs)
-[![](https://img.shields.io/badge/Day_12-☆-black)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_12.mjs)
+[![](https://img.shields.io/badge/Day_12-★-gold)](https://github.com/MilanFox/Advent-of-Code/blob/main/src/2025/day_12.mjs)
 
 ## 🏺️ [2024](https://adventofcode.com/2024) ![](https://progress-bar.xyz/34?scale=49&suffix=/49)
 
---
+++ src/2025/day_12.mjs
@@ -0,0 +1,48 @@
+import { readFileSync } from 'node:fs';
+
+class Shape {
+  constructor(data) {
+    this.id = data[0];
+    this.map = data.split('\n').slice(1).map(line => line.split(''));
+    this.area = this.map.reduce((acc, cur) => acc + cur.filter(cell => cell === '#').length, 0);
+  }
+}
+
+class Region {
+  constructor(data, shapes) {
+    const [sizeData, shapeData] = data.split(': ');
+    [this.width, this.height] = sizeData.split('x').map(Number);
+    this.requiredShapes = shapeData.split(' ').map((amount, id) => ({ amount, shape: shapes[id] }));
+  }
+
+  get totalArea() {
+    return this.width * this.height;
+  }
+
+  get totalShapesArea() {
+    return this.requiredShapes.reduce((acc, { amount, shape }) => acc + (amount * shape.area), 0);
+  }
+
+  get isImpossible() {
+    return this.totalArea < this.totalShapesArea;
+  }
+
+  get isTrivial() {
+    const numberOfCells = Math.floor(this.width / 3) * Math.floor(this.height / 3);
+    const neededCells = this.requiredShapes.reduce((acc, { amount }) => acc + Number(amount), 0);
+    return numberOfCells >= neededCells;
+  };
+}
+
+const inputData = readFileSync('input.txt', 'utf-8').trim().split('\n\n');
+const regionData = inputData.pop();
+
+const shapes = inputData.map(data => new Shape(data));
+const regions = regionData.split('\n').map(data => new Region(data, shapes));
+
+// Wtf?
+const possibleRegions = regions.reduce((acc, cur) => acc + cur.isTrivial, 0);
+const trivialRegions = regions.reduce((acc, cur) => acc + cur.isTrivial, 0);
+
+if (possibleRegions === trivialRegions) {}
+console.log(`Part 1: ${trivialRegions}`);
